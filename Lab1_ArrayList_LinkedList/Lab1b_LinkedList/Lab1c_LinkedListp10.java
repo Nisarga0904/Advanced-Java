3933508e8c5efb174a9af23a83a8a995678449ca
Nisarga S <120187734+Nisarga0904@users.noreply.github.com>
2025-05-22 21:20:41 +0530

    Update Lab1c_LinkedListp10.java

--- Lab1_ArrayList_LinkedList/Lab1b_LinkedList/Lab1c_LinkedListp10.java
+++ Lab1_ArrayList_LinkedList/Lab1b_LinkedList/Lab1c_LinkedListp10.java
@@ -1,7 +1,7 @@
 // Write a Java program that swaps two elements ( first and third elements ) in a linked list ( using
 // Collections.swap(l_list, 0, 2))
 import java.util.*;
-public class LinkedListp10 {
+public class Lab1c_LinkedListp10 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		LinkedList<String> ls = new LinkedList<>();
